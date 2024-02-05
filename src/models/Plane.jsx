cec14bb4984674e4f909f7b2423da51f0ec59b6c
Safkat khan Tafsir <safkatkhan420@gmail.com>
2024-02-06 05:27:33 +0600

    Plane & Bird Added

---
+++ src/models/Plane.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import  planeScene from '../assets/3d/plane.glb';
+const Plane = () => {
+    const { scene, animations } = useGLTF(planeScene);
+  return (
+    <mesh>
+    <primitive object={scene} />
+       </mesh>
+  )
+}
+
+export default Plane
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,29 @@
 import { Suspense} from 'react'
 import { Canvas } from '@react-three/fiber'
 import Loader from '../components/Loader'
+import  Island  from '../models/Island';
+import { Plane, Sky } from '@react-three/drei';
+import Bird from '../models/Bird';
  {/* <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center ">POPUP</div>*/}
 
 const Home = () => {
+  const adjustIslanForScreenSize = () => {
+    let screenScale = null;
+    let screenPostion  = [0, -6.5, -43];
+    let rotation = [0.1, 4.7, 0];
+
+    if(window.innerWidth < 768 ){
+      screenScale = [0.9, 0.9, 0.9];
+    }
+    else{
+      screenScale = [1, 1, 1];
+    }
+
+    return [screenScale,screenPostion,rotation];
+  }
+
+  const [islandScale,islandPosition,islandRotation] = adjustIslanForScreenSize();
+
   return (
 <section className="w-full h-screen relative">
 
@@ -11,12 +31,19 @@ const Home = () => {
 camera={{near: 0.1 , far : 1000}}
 >
 <Suspense fallback = {<Loader />}>
-<directionalLight />
-<ambientLight />
-<pointLight />
-<spotLight />
-<hemisphereLight />
+<directionalLight position={[1,1,1]} intensity={2} />
+<ambientLight intensity={0.5} />
+<hemisphereLight skyColor="#b1e1ff" groundColor="#000000" intensity={1} />
 
+<Plane />
+<Bird />
+<Sky />
+<Island 
+position = {islandPosition}
+scale = {islandScale}
+rotation = {islandRotation}
+
+/>
 </Suspense>
 </Canvas>
 
