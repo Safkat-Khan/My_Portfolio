1632afe21d336c9e89b8f03252cf6a883eecd6d6
Safkat khan Tafsir <safkatkhan420@gmail.com>
2024-02-12 18:16:38 +0600

    Portfolio Complete

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { Suspense, useState} from 'react'
+import { Suspense, useState, useRef,useEffect} from 'react'
 import { Canvas } from '@react-three/fiber'
 import Loader from '../components/Loader'
 
@@ -7,11 +7,25 @@ import  Sky  from '../models/Sky';
 import Bird from '../models/Bird';
 import Plane from '../models/Plane';
 import HomeInfo from '../components/HomeInfo';
- {/* <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center ">POPUP</div>*/}
+import sakura from '../assets/sakura.mp3'
+import { soundoff, soundon } from '../assets/icons';
 
 const Home = () => {
+  const audioRef = useRef(new Audio(sakura));
+ audioRef.current.volume = 0.5; 
+ audioRef.current.loop = true;
   const [isRotating,setIsRotating] = useState(false);
   const [currentStage,setCurrentStage] = useState(1);
+  const  [isPlayingMusic, setIsPlayingMusic] = useState(true);
+
+  useEffect(() =>{
+    if(isPlayingMusic){
+      audioRef.current.play();
+    }
+    return() => {
+      audioRef.current.pause();
+    }
+  },[isPlayingMusic])
 
   const adjustIslanForScreenSize = () => {
     let screenScale = null;
@@ -84,6 +98,13 @@ rotation={[0, 20, 0]}
 
 </Suspense>
 </Canvas>
+
+<div className="absolute bottom-2 left-2">
+  <img src={!isPlayingMusic? soundoff : soundon} alt="sound"
+  className="w-10 h-10 cursor-pointer object-contain" 
+  onClick = {() => setIsPlayingMusic(!isPlayingMusic)}
+  />
+</div>
 
 </section>
   )
